74f3e79a3464e5cc81a02b7c2a2a9183c37f34b1
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-07 12:29:57 -0600

    Add beers to welcome page, beers index page, and beer database

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index"
 
-  get '/breweries', to: 'breweries#index'
-  get '/breweries/new', to: 'breweries#new'
-  post '/breweries', to: 'breweries#create'
-  get '/breweries/:id', to: 'breweries#show'
-  get '/breweries/:id/edit', to: 'breweries#edit'
-  patch 'breweries/:id', to: 'breweries#update'
-  delete '/breweries/:id', to: 'breweries#destroy'
+  get "/breweries", to: "breweries#index"
+  get "/breweries/new", to: "breweries#new"
+  post "/breweries", to: "breweries#create"
+  get "/breweries/:id", to: "breweries#show"
+  get "/breweries/:id/edit", to: "breweries#edit"
+  patch "breweries/:id", to: "breweries#update"
+  delete "/breweries/:id", to: "breweries#destroy"
+
+  get "/beers", to: "beers#index"
 end
