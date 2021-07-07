9d10c9be37127ba6996453e668407a7d9d685398
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-07 17:37:46 -0600

    Add test for user story 4

--- spec/features/beers/show_spec.rb
+++ spec/features/beers/show_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe 'the beer show page' do
                         abv: 7.0,
                         ibu: 55)
     visit "/beers/#{beer.id}"
+    save_and_open_page
 
+    expect(page).to have_content("Beer Index")
+    expect(page).to have_content(beer.name)
   end
 
   it 'displays the name of brewery for the beer' do
