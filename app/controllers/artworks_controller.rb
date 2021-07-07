989354dd7af643c0b6dd22fdce08138f03e6f5ca
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-07 09:39:34 -0700

    Add Artwork create method

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,19 @@
-class ArtworksController < Application
+class ArtworksController < ApplicationController
   def index
+    @artworks = Artwork.all
+  end
+
+  def new
+  end
+
+  def create
+    artwork = Artwork.new({
+      title: params[:artwork][:title],
+      description: params[:artwork][:description]
+      })
+
+      artwork.save
+
+      redirect_to '/artworks'
   end
 end
