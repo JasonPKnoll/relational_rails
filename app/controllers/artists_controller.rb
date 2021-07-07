2fd0449fe7a6421bd567ce7b5b4178887fa50b14
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-07 09:38:26 -0700

    Add artist create method

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,19 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = ['artist 1', 'artist 2']
   end
-end
+
+  def new
+  end
+
+  def create
+    artist = Artist.new({
+      name: params[:artist][:name],
+      description: params[:artist][:description]
+      })
+
+      artist.save
+
+      redirect_to '/artists'
+    end
+  end
