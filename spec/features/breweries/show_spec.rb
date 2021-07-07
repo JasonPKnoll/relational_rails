df20dcd2d4328bf4a6020af0d7c854ca8ef3cb69
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-07 12:11:37 -0600

    Set up test files

---
+++ spec/features/breweries/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'the breweries show page' do
+  it 'displays the beer name' do
+
+  end
+
+  it 'displays the name of brewery for the song' do
+    
+  end
+end
