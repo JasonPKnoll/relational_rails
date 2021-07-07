94902a76f93aa66e55bd7d202c3c6a33e387300f
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-06 20:29:43 -0600

    Fix the bug

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -2,4 +2,19 @@ class BreweriesController < ApplicationController
   def index
     @breweries = ['Brewery 1', 'Brewery 2']
   end
+
+  def new
+  end
+
+  def create
+    require 'pry'; binding.pry
+    brewery = Brewery.new({
+      name: params[:brewery][:name],
+      year: params[:brewery][:year]
+      })
+    require 'pry'; binding.pry
+    brewery.save
+    require 'pry'; binding.pry
+    redirect_to "/breweries"
+  end
 end
