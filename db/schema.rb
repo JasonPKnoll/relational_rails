0e7e673addc50474d595bc42a5a5f5a15a9c8ada
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-07 18:31:14 -0600

    Create the connection between beers and breweries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_182756) do
+ActiveRecord::Schema.define(version: 2021_07_07_234708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_07_07_182756) do
     t.integer "ibu"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "brewery_id"
+    t.index ["brewery_id"], name: "index_beers_on_brewery_id"
   end
 
   create_table "breweries", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_07_07_182756) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "beers", "breweries"
 end
--- spec/features/beers/show_spec.rb
+++ spec/features/beers/show_spec.rb
@@ -1,19 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe 'the beer show page' do
+RSpec.describe 'the beers show page' do
   it 'displays the beer name' do
-    beer = Beer.create!(name: "Two Hearted Ale",
+    brewery = Brewery.create!(name: "Bells Brewery",
+                              location: "Kalamazoo, MI",
+                              year_established: 1985,
+                            )
+    beer = brewery.beers.create!(name: "Two Hearted Ale",
                         style: "American IPA",
                         abv: 7.0,
-                        ibu: 55)
+                        ibu: 55
+                      )
+    beer2 = brewery.beers.create!(name: "Oberon Ale",
+                        style: "American Pale Wheat",
+                        abv: 5.8,
+                        ibu: 0
+                      )
     visit "/beers/#{beer.id}"
     save_and_open_page
 
     expect(page).to have_content("Beer Index")
     expect(page).to have_content(beer.name)
+    expect(page).to_not have_content(beer2.name)
   end
 
   it 'displays the name of brewery for the beer' do
+    brewery = Brewery.create!(name: "Bells Brewery",
+                              location: "Kalamazoo, MI",
+                              year_established: 1985,
+                            )
+    beer = brewery.beers.create!(name: "Two Hearted Ale",
+                        style: "American IPA",
+                        abv: 7.0,
+                        ibu: 55
+                      )
+    beer2 = brewery.beers.create!(name: "Oberon Ale",
+                        style: "American Pale Wheat",
+                        abv: 5.8,
+                        ibu: 0
+                      )
+    visit "/beers/#{beer.id}"
+    save_and_open_page
 
+    expect(page).to have_content(brewery.name)
   end
 end
