a6f42449e63e231e0e9ad3f05195fb4f38bee295
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-08 13:11:59 -0700

    Add story 1 and 2 to artist spec

---
+++ spec/features/artists/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'the artists show page' do
+
+  # => Story 1
+  # => As a visitor
+  # => When I visit '/artists'
+  # => I see the name of each artist record in the system
+  it 'displays all artists names' do
+    artist = Artist.create!(name: "Povi", description: "From scratch avatar creator", years_experience: 10, comissions_open: false)
+    visit "/artists" #localhost:3000/artist
+
+    expect(page).to have_content(artist.name)
+  end
+
+  # => Story 2
+  # => As a visitor
+  # => When I visit '/artists/:id'
+  # => Then I see the artist with that id including the artists attributes
+  it 'displays the artists specified by id and all its attributes' do
+    artist = Artist.create!(name: "Povi", description: "From scratch avatar creator", years_experience: 10, comissions_open: false)
+    artist_2 = Artist.create!(name: "lincoln", description: "3D artist", years_experience: 1, comissions_open: false)
+    visit "/artists/#{artist.id}" #localhost:3000/artist/1
+
+    expect(page).to have_content(artist.name)
+    expect(page).to_not have_content(artist_2.name)
+  end
+end
