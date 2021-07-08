5fd8834cbd2fa8b30691aca47e74b7969da7abe4
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-08 06:32:24 -0700

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "welcome#index"
-  get '/artists', to: 'artists#index'
-  get '/artworks', to: 'artwork#index'
+  get '/', to: 'welcome#index'
 
+  get '/artists', to: 'artists#index'
+
+  get '/artists/new', to: 'artists#new'
+  post '/artists', to: 'artists#create'
+
+  get '/artists/:id', to: 'artists#show'
+
+  get '/artworks', to: 'artworks#index'
+
+  get 'artworks/new', to: 'artworks#new'
+  post 'artworks', to: 'artworks#create'
+
+  get '/easteregg', to: 'eastereggs#index'
 end
