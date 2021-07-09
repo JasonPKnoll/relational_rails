d175edd6cdf5479de41a391b3a6ad166789d5d17
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-08 18:57:58 -0700

    Refactor artists index

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -2,17 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'the artists show page' do
 
-  # => Story 1
-  # => As a visitor
-  # => When I visit '/artists'
-  # => I see the name of each artist record in the system
-  it 'displays all artists names' do
-    artist = Artist.create!(name: "Povi", description: "From scratch avatar creator", years_experience: 10, comissions_open: false)
-    visit "/artists" #localhost:3000/artist
-
-    expect(page).to have_content(artist.name)
-  end
-
   # => Story 2
   # => As a visitor
   # => When I visit '/artists/:id'
