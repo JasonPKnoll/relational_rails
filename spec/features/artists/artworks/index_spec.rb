4c9b814f48c739e800f8bf8347c05750561831db
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-09 15:35:53 -0700

    Reorganize test artworks and artists

--- spec/features/artists/artworks/index_spec.rb
+++ spec/features/artists/artworks/index_spec.rb
@@ -7,9 +7,21 @@ RSpec.describe 'Artists songs index' do
   # => When I visit '/artists/artist_id/artwork'
   # => I then see artwork that is associated with that artist with all artworks attributes
   it 'displays specified artwork of that artist based on their id' do
-    povi = Artist.create!(name: "Povi", description: "From scratch avatar creator", years_experience: 10, comissions_open: false)
-    povichi = povi.artworks.create!(name: "Povichi", art_type: "3D Base Model", price: 32, for_sale: true)
-    kemononmimi = povi.artworks.create!(name: "Kemonomimi", art_type: "3D Base Model", price: 45, for_sale: true)
+    povi = Artist.create!(name: "Povi",
+                          description: "From scratch avatar creator",
+                          years_experience: 10,
+                          comissions_open: false)
+
+    povichi = povi.artworks.create!(name: "Povichi",
+                                    art_type: "3D Base Model",
+                                    price: 32,
+                                    for_sale: true)
+
+    kemononmimi = povi.artworks.create!(name: "Kemonomimi",
+                                        art_type: "3D Base Model",
+                                        price: 45,
+                                        for_sale: true)
+
     visit "/artists/#{povi.id}/artworks"
 
     expect(page).to have_content(povichi.name)
--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -7,8 +7,16 @@ RSpec.describe 'the artists show page' do
   # => When I visit '/artists/:id'
   # => Then I see the artist with that id including the artists attributes
   it 'displays the artists specified by id and all its attributes' do
-    artist = Artist.create!(name: "Povi", description: "From scratch avatar creator", years_experience: 10, comissions_open: false)
-    artist_2 = Artist.create!(name: "Lincoln", description: "3D artist", years_experience: 1, comissions_open: false)
+    artist = Artist.create!(name: "Povi",
+                            description: "From scratch avatar creator",
+                            years_experience: 10,
+                            comissions_open: false)
+
+    artist_2 = Artist.create!(name: "Lincoln",
+                              description: "3D artist",
+                              years_experience: 1,
+                              comissions_open: false)
+                              
     visit "/artists/#{artist.id}" #localhost:3000/artist/1
 
     expect(page).to have_content(artist.name)
