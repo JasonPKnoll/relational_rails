2bce0d8b6124c2dc6606d81e079403b591f7edd2
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-09 14:29:36 -0700

    Update story 2 test

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -8,10 +8,16 @@ RSpec.describe 'the artists show page' do
   # => Then I see the artist with that id including the artists attributes
   it 'displays the artists specified by id and all its attributes' do
     artist = Artist.create!(name: "Povi", description: "From scratch avatar creator", years_experience: 10, comissions_open: false)
-    artist_2 = Artist.create!(name: "lincoln", description: "3D artist", years_experience: 1, comissions_open: false)
+    artist_2 = Artist.create!(name: "Lincoln", description: "3D artist", years_experience: 1, comissions_open: false)
     visit "/artists/#{artist.id}" #localhost:3000/artist/1
 
     expect(page).to have_content(artist.name)
+    expect(page).to have_content(artist.description)
+    expect(page).to have_content(artist.years_experience)
+    expect(page).to have_content(artist.comissions_open)
+    expect(page).to have_content(artist.created_at)
+    expect(page).to have_content(artist.updated_at)
+
     expect(page).to_not have_content(artist_2.name)
   end
 end
