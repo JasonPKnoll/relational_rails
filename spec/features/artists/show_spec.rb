12bed2bb14af890bdfeb96521a3754ad151dba24
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-10 14:48:41 -0700

    Add story 7 test in artist show spec

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'the artists show page' do
                               description: "3D artist",
                               years_experience: 1,
                               comissions_open: false)
-                              
+
     visit "/artists/#{artist.id}" #localhost:3000/artist/1
 
     expect(page).to have_content(artist.name)
@@ -27,5 +27,33 @@ RSpec.describe 'the artists show page' do
     expect(page).to have_content(artist.updated_at)
 
     expect(page).to_not have_content(artist_2.name)
+  end
+
+  it 'Shows the count of artworks for artist' do
+
+    # => Story 7
+    # => As a visitor
+    # => When I visit '/artists/artist_id'
+    # => I see a count of the number of artworks associated with this Artist
+
+    povi = Artist.create!(name: "Povi",
+                          description: "From scratch avatar creator",
+                          years_experience: 10,
+                          comissions_open: false)
+
+    artwork = povi.artworks.create!(name: "Povichi",
+                                    art_type: "3D Base Model",
+                                    price: 32,
+                                    for_sale: true)
+
+    artwork_2 = povi.artworks.create!(name: "Kemonomimi",
+                                      art_type: "3D Base Model",
+                                      price: 45,
+                                      for_sale: true)
+
+    visit "/artists/#{povi.id}"
+    save_and_open_page
+
+    expect(page).to have_content("Total works of art for Povi: 2")
   end
 end
