25c62dcba691f055609a7c7f389e3d7da9ffae49
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-10 21:36:20 -0700

    Add fuctions to artists view new

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,36 +7,37 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    artist = Artist.new({
+    artist = Artist.create({
       name: params[:artist][:name],
       description: params[:artist][:description],
       years_experience: params[:artist][:years_experience],
       comissions_open: params[:artist][:comissions_open]
       })
 
-      artist.save
-
-      redirect_to '/artists'
-    end
-
-    def show
-      @artist = Artist.find(params[:id])
-    end
-
-    def update
-      artist = Artist.find(params[:id])
-      artist.update({
-        name: params[:artist][:name],
-        description: params[:artist][:description],
-        years_experience: params[:artist][:years_experience],
-        comissions_open: params[:artist][:comissions_open]
-        })
-        artist.save
-        redirect_to "/artists/#{artist.id}"
-    end
-
-    def destroy
-      Artist.destroy(params[:id])
-      redirect_to '/artists'
-    end
+    redirect_to "/artists"
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(artist_params)
+    artist.save
+
+    redirect_to "/artists/#{artist.id}"
+  end
+
+  def destroy
+    Artist.destroy(params[:id])
+    redirect_to '/artists'
+  end
+
+  def artist_params
+    params.permit(:name,
+                  :description,
+                  :years_experience,
+                  :comissions_open)
+  end
+end
