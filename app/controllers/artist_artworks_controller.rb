7aff1a27c8cb1b6f226517176d6dde97fb6e1bfa
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-12 15:21:58 -0700

    Add create method to artist artworks controller

--- app/controllers/artist_artworks_controller.rb
+++ app/controllers/artist_artworks_controller.rb
@@ -1,6 +1,32 @@
 class ArtistArtworksController < ApplicationController
   def index
-    artist = Artist.find(params[:id])
-    @artworks = artist.artworks
+    @artist = Artist.find(params[:id])
+    @artworks = @artist.artworks
+  end
+
+  def new
+  end
+
+  def create
+    @artist = Artist.find(params[:id])
+    artwork = @artist.artworks.create(artwork_params)
+    artwork.save
+    redirect_to "/artists/#{artist.id}"
+  end
+
+  def update
+    @artist.artworks.create(artwork_params)
+
+    redirect_to "/artworks/#{artwork.id}"
+  end
+
+  def destroy
+    Artwork.destroy(params[:id])
+    redirect_to '/artworks'
+  end
+
+  private
+  def artwork_params
+    params.permit(:name, :art_type, :price, :for_sale)
   end
 end
